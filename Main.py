dfa4604cd7905213eab59d7f657c9bb4fed1fefe
ahsanhassan999 <s.ahsanhassanrizvi2004@gmail.com>
2024-06-28 23:10:46 +0500

    .

--- Main.py
+++ Main.py
@@ -1,7 +1,6 @@
 # Employee Management System Using Python - Sagar Developer
 
 
-import re
 # importing mysql connector
 import mysql.connector
 
@@ -9,11 +8,7 @@ import mysql.connector
 con = mysql.connector.connect(
     host="localhost", user="root", password="", database="Employee")
 
-# make a regular expression
-# for validating an Email
-regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
-# for validating an Phone Number
-Pattern = re.compile("(0|91)?[7-9][0-9]{9}")
+
 
 
 # Function to Add_Employ
@@ -32,17 +27,7 @@ def Add_Employ():
         press = input("Press Any Key To Continue..")
         Add_Employ()
     Email_Id = input("Enter Employee Email ID: ")
-    if (re.fullmatch(regex, Email_Id)):
-        print("Valid Email")
-    else:
-        print("Invalid Email")
-        press = input("Press Any Key To Continue..")
-        Add_Employ()
     Phone_no = input("Enter Employee Phone No.: ")
-
-    print("Valid Phone Number")
-
-
     Address = input("Enter Employee Address: ")
     Post = input("Enter Employee Post: ")
     Salary = input("Enter Employee Salary: ")
@@ -146,19 +131,7 @@ def Update_Employ():
         menu()
     else:
         Email_Id = input("Enter Employee Email ID: ")
-        if (re.fullmatch(regex, Email_Id)):
-            print("Valid Email")
-        else:
-            print("Invalid Email")
-            press = input("Press Any Key To Continue..")
-            Update_Employ()
         Phone_no = input("Enter Employee Phone No.: ")
-        if (Pattern.match(Phone_no)):
-            print("Valid Phone Number")
-        else:
-            print("Invalid Phone Number")
-            press = input("Press Any Key To Continue..")
-            Update_Employ()
         Address = input("Enter Employee Address: ")
         # Updating Employee details in empdata Table
         sql = 'UPDATE Employee_Data set Email_Id = %s, Phone_no = %s, Address = %s where Id = %s'
@@ -177,7 +150,7 @@ def Update_Employ():
 
 # Function to Promote_Employ
 def Promote_Employ():
-    print("{:>60}".format("-->> Promote Employee Record <<--\n"))
+    print("{:>60}".format("-->> Promote/Demote Employee Record <<--\n"))
     Id = input("Enter Employee Id: ")
     # checking If Employee Id is Exit Or Not
     if (check_employee(Id) == False):
@@ -185,29 +158,41 @@ def Promote_Employ():
         press = input("Press Any Key To Continue..")
         menu()
     else:
-        Amount = int(input("Enter Increase Salary: "))
+        sql = 'select * from Employee_Data'
+        c = con.cursor()
+
+        # Executing the sql query
+        c.execute(sql)
+
+        # Fetching all details of all the Employees
+        r = c.fetchone()
+
+        print("1.Employee Id: ", r[0])
+        print("2.Employee Name: ", r[1])
+        print("3.Employee Email Id: ", r[2])
+        print("4.Employee Phone No.: ", r[3])
+        print("5.Employee Address: ", r[4])
+        print("6.Employee Post: ", r[5])
+        print("7.Employee Salary: ", r[6])
+        print("\n")
+        Choice = input("Which Field you want to edit: ")
+        
+
+
+
+        Post = input("Enter New Post: ")
+        Amount = int(input("Enter New Salary: "))
+
         # query to fetch salary of Employee with given data
-        sql = 'select Salary from Employee_Data where Id=%s'
-        data = (Id,)
+        sql = 'UPDATE Employee_Data set Post = %s, Salary = %s where Id = %s'
+        data = (Post, Amount, Id)
         c = con.cursor()
 
         # executing the sql query
         c.execute(sql, data)
 
-        # fetching salary of Employee with given Id
-        r = c.fetchone()
-        t = r[0] + Amount
-
-        # query to update salary of Employee with given id
-        sql = 'update Employee_Data set Salary = %s where Id = %s'
-        d = (t, Id)
-
-        # executing the sql query
-        c.execute(sql, d)
-
-        # commit() method to make changes in the table
         con.commit()
-        print("Employee Promoted")
+        print("Employee Promoted/Demoted")
         press = input("Press Any key To Continue..")
         menu()
 
@@ -277,8 +262,8 @@ def menu():
     print("{:>60}".format("************************************"))
     print("1. Add Employee")
     print("2. Display Employee Record")
-    print("3. Update Employee Record")
-    print("4. Promote Employee Record")
+    print("3. Update Employee Details")
+    print("4. Promote/Demote Employee Record")
     print("5. Remove Employee Record")
     print("6. Search Employee Record")
     print("7. Exit\n")
